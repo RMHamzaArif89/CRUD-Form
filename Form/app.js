d23517c96a36905154e4d09a796c7a2a3ccc9c3e
RMHamzaArif89 <ranaarifisonline@gmail.com>
2024-03-27 05:04:28 +0500

    first commit

---
+++ Form/app.js
@@ -0,0 +1,26 @@
+const express=require('express')
+const app=express()
+const bodyParser=require('body-parser')
+const path=require('path')
+const exp = require('constants')
+
+
+//middleware
+app.use(express.static('static'))
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+
+//set the view engine
+app.set('view engine','ejs')
+app.set('views','views')
+
+
+app.get('/',(req,res)=>{
+    res.render('index')
+})
+
+
+app.listen(3000,()=>{
+    console.log('port is listening')
+})
